a54deef2e5a394f2b27a8a6219c3bba7663608e1
Sarmad Sohail <sarmad-sohail@hotmail.com>
2022-06-17 01:02:13 +0500

    dynamic listing and book store

--- resources/views/dashboard/books.blade.php
+++ resources/views/dashboard/books.blade.php
@@ -36,13 +36,14 @@
         </div>
         <div class="block-element">
             <div class="custom-slider2 arrows-2">
-                <div class="books-box-wrapper">
+                @foreach($books as $book)
+                    <div class="books-box-wrapper">
                     <div class="books-box2">
                         <div class="books-box-image">
                             <img src="{{asset('assets/images/book-new7.png')}}">
                         </div>
                         <div class="books-box-title">
-                            <div> Twice Shy</div>
+                            <div> {{$book->title}}</div>
                             <div class="reviews">
                                 <i class="fa fa-star star-onn"> </i>
                                 <i class="fa fa-star star-onn"> </i>
@@ -54,142 +55,12 @@
                         </div>
                         <div class="books-box-desc">
                             <h6> By John Ven </h6>
-                            <p> Spent lounging on the beach, madly in love friend </p>
+                            <p> {{$book->description}} </p>
                         </div>
                     </div>
                 </div>
-                <div class="books-box-wrapper">
-                    <div class="books-box2">
-                        <div class="books-box-image">
-                            <img src="{{asset('assets/images/book-new8.png')}}">
-                        </div>
-                        <div class="books-box-title">
-                            <div> Love At First</div>
-                            <div class="reviews">
-                                <i class="fa fa-star star-onn"> </i>
-                                <i class="fa fa-star star-onn"> </i>
-                                <i class="fa fa-star star-onn"> </i>
-                                <i class="fa fa-star star-onn"> </i>
-                                <i class="fa fa-star star-off"> </i>
-                                <b class="col-black"> (46) </b>
-                            </div>
-                        </div>
-                        <div class="books-box-desc">
-                            <h6> By John Ven </h6>
-                            <p> Spent lounging on the beach, madly in love friend </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="books-box-wrapper">
-                    <div class="books-box2">
-                        <div class="books-box-image">
-                            <img src="{{asset('assets/images/book-new9.png')}}">
-                        </div>
-                        <div class="books-box-title">
-                            <div> The Check</div>
-                            <div class="reviews">
-                                <i class="fa fa-star star-onn"> </i>
-                                <i class="fa fa-star star-onn"> </i>
-                                <i class="fa fa-star star-onn"> </i>
-                                <i class="fa fa-star star-onn"> </i>
-                                <i class="fa fa-star star-off"> </i>
-                                <b class="col-black"> (46) </b>
-                            </div>
-                        </div>
-                        <div class="books-box-desc">
-                            <h6> By John Ven </h6>
-                            <p> Spent lounging on the beach, madly in love friend </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="books-box-wrapper">
-                    <div class="books-box2">
-                        <div class="books-box-image">
-                            <img src="{{asset('assets/images/book-new10.png')}}">
-                        </div>
-                        <div class="books-box-title">
-                            <div> Love At First</div>
-                            <div class="reviews">
-                                <i class="fa fa-star star-onn"> </i>
-                                <i class="fa fa-star star-onn"> </i>
-                                <i class="fa fa-star star-onn"> </i>
-                                <i class="fa fa-star star-onn"> </i>
-                                <i class="fa fa-star star-off"> </i>
-                                <b class="col-black"> (46) </b>
-                            </div>
-                        </div>
-                        <div class="books-box-desc">
-                            <h6> By John Ven </h6>
-                            <p> Spent lounging on the beach, madly in love friend </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="books-box-wrapper">
-                    <div class="books-box2">
-                        <div class="books-box-image">
-                            <img src="{{asset('assets/images/book-new11.png')}}">
-                        </div>
-                        <div class="books-box-title">
-                            <div> The Check</div>
-                            <div class="reviews">
-                                <i class="fa fa-star star-onn"> </i>
-                                <i class="fa fa-star star-onn"> </i>
-                                <i class="fa fa-star star-onn"> </i>
-                                <i class="fa fa-star star-onn"> </i>
-                                <i class="fa fa-star star-off"> </i>
-                                <b class="col-black"> (46) </b>
-                            </div>
-                        </div>
-                        <div class="books-box-desc">
-                            <h6> By John Ven </h6>
-                            <p> Spent lounging on the beach, madly in love friend </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="books-box-wrapper">
-                    <div class="books-box2">
-                        <div class="books-box-image">
-                            <img src="{{asset('assets/images/book-new12.png')}}">
-                        </div>
-                        <div class="books-box-title">
-                            <div> Twice Shy</div>
-                            <div class="reviews">
-                                <i class="fa fa-star star-onn"> </i>
-                                <i class="fa fa-star star-onn"> </i>
-                                <i class="fa fa-star star-onn"> </i>
-                                <i class="fa fa-star star-onn"> </i>
-                                <i class="fa fa-star star-off"> </i>
-                                <b class="col-black"> (46) </b>
-                            </div>
-                        </div>
-                        <div class="books-box-desc">
-                            <h6> By John Ven </h6>
-                            <p> Spent lounging on the beach, madly in love friend </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="books-box-wrapper">
-                    <div class="books-box2">
-                        <div class="books-box-image">
-                            <img src="{{asset('assets/images/book-new8.png')}}">
-                        </div>
-                        <div class="books-box-title">
-                            <div> Twice Shy</div>
-                            <div class="reviews">
-                                <i class="fa fa-star star-onn"> </i>
-                                <i class="fa fa-star star-onn"> </i>
-                                <i class="fa fa-star star-onn"> </i>
-                                <i class="fa fa-star star-onn"> </i>
-                                <i class="fa fa-star star-off"> </i>
-                                <b class="col-black"> (46) </b>
-                            </div>
-                        </div>
-                        <div class="books-box-desc">
-                            <h6> By John Ven </h6>
-                            <p> Spent lounging on the beach, madly in love friend </p>
-                        </div>
-                    </div>
-                </div>
+                @endforeach
+
             </div>
         </div>
     </div>
@@ -526,11 +397,13 @@
     <!-- Add Book Popup Starts Here -->
     <div class="modal fade" id="modal-1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
          aria-hidden="true">
-        <div class="modal-dialog" role="document" style="max-width: 520px;">
+        <form action="{{route('book.store')}}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="modal-dialog" role="document" style="max-width: 520px;">
             <div class="modal-content popup-wrapper">
                 <div class="popup-head text-center">
                     <div class="add-group-name-field">
-                        <input class="field-style1" type="text" placeholder="TYPE BOOK TITLE" name="">
+                        <input class="field-style1" type="text" placeholder="TYPE BOOK TITLE" name="title" required>
                     </div>
                     <button type="button" class="close close-btn1" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
@@ -544,33 +417,33 @@
                             <div class="block-element2">
                                 <div class="form-field2">
                                     <label> Category </label>
-                                    <select class="field-style2">
-                                        <option> Science</option>
-                                        <option> Technology</option>
+                                    <select class="field-style2" name="category" required>
+                                        <option value="science"> Science</option>
+                                        <option value="technology"> Technology</option>
                                     </select>
                                 </div>
 
                                 <div class="form-field2 image-uploader-wrap">
                                     <label> Upload Cover: </label>
-                                    <input type="file" id="file-1" accept=".jpg,.jpeg,.png,.svg"/>
+                                    <input type="file" id="file-1" accept=".jpg,.jpeg,.png,.svg" name="cover"/>
                                     <label for="file-1"> <i class="fa fa-plus"> </i> choose a file</label>
                                 </div>
 
                                 <div class="form-field2 image-uploader-wrap">
                                     <label> Upload PDF: </label>
-                                    <input type="file" id="file-2" accept=".pdf"/>
+                                    <input type="file" id="file-2" accept=".pdf" name="pdf"/>
                                     <label for="file-2"> <i class="fa fa-plus"> </i> choose a file</label>
                                 </div>
 
                                 <div class="form-field2 image-uploader-wrap">
                                     <label> Upload Audio: </label>
-                                    <input type="file" id="file-3" accept=".mp3"/>
+                                    <input type="file" id="file-3" accept=".mp3" name="audio"/>
                                     <label for="file-3"> <i class="fa fa-plus"> </i> choose a file</label>
                                 </div>
 
                                 <div class="form-field3">
                                     <label> Description: </label>
-                                    <textarea class="field-style3"></textarea>
+                                    <textarea class="field-style3" name="description" required></textarea>
                                 </div>
 
                             </div>
@@ -589,6 +462,7 @@
 
             </div>
         </div>
+        </form>
     </div>
     <!-- Add Book Popup Ends Here -->
 
